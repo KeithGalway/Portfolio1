a4f5056ea552b8b02810729ad8e7f6651c8b3152
KeithGalway <keith.galway@outlook.com>
2024-03-23 02:08:58 +0800

    Added backend
    Initiated node project
    Import express
    Create express server to server static files in public folder
    Move assets into public folder
    Move pages into view folder
    Add 404 page
    Added gitignore file to ignore node_modules
    Tested on Chrome to be working

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 3500
+
+const app = express()
+
+// serve static files
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.get('^/$|/index(.html)?', (req, res) => {
+    // res.sendFile('./views/index.html', { root: __dirname })
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/about(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
+app.get('/services(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'services.html'))
+})
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ error: '404 Not Found'})
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
